be9fc6ddafcd4ca772877b0e653db91693867d32
yukitosi <t.yuki.0527@gmail.com>
2021-04-06 19:21:54 +0900

    prototype
    教えてもらいます

---
+++ Assets/Scriots/goalscropt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goalscropt : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("判定");
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scriots/player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    [Range(0f, 20f)] public float speed = 10;
+    
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //Rigidbodyに力を加える
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(movement * speed);
+
+        
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
